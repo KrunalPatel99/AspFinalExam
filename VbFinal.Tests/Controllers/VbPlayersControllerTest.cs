df26fbf1b81998fb573f36c7fc859a77c37a9d82
KRUNAL PATEL <kp14127@gmail.com>
2019-04-17 13:36:56 -0400

    Submitting

---
+++ VbFinal.Tests/Controllers/VbPlayersControllerTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using VbFinal.Controllers;
+using VbFinal.Models;
+
+namespace VbFinal.Tests.Controllers
+{
+    [TestClass]
+    public class VbPlayersControllerTest
+    {
+        VbPlayersController controller;
+        Mock<IMockVbPlayer> mock;
+        List<VbPlayer> vbPlayers;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            mock = new Mock<IMockVbPlayer>();
+
+            vbPlayers = new List<VbPlayer>
+            {
+                new VbPlayer
+                {
+                    VbPlayerId = 961,
+                    FirstName = "Lori",
+                    Lastname = "S",
+                    Photo = "https://img.icons8.com/color/384/beach-volleyball.png"
+                },
+                new VbPlayer
+                {
+                    VbPlayerId = 961,
+                    FirstName = "Zak",
+                    Lastname = "N",
+                    Photo = "https://img.icons8.com/ultraviolet/384/beach-volleyball.png"
+                }
+            };
+
+            mock.Setup(m => m.VbPlayers).Returns(vbPlayers.AsQueryable());
+            controller = new VbPlayersController(mock.Object);
+        }
+
+        [TestMethod]
+        public void IndexViewLoads()
+        {
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+        public void IndexValidLoadsVbPlayers()
+        {
+
+            var result = (List<VbPlayer>)((ViewResult)controller.Index()).Model;
+
+            CollectionAssert.AreEqual(vbPlayers.OrderBy(c => c.VbPlayerId).ToList(), result);
+        }
+     
+    }
+
+}
